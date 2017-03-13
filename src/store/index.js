278cc4bbf5156847943e7083123029720d91bb94
Hilton Shumway <hillshum@gmail.com>
2017-03-13 16:00:20 -0600

    WIP on the optimistic middleware

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware, bindActionCreators} from 'redux'
 import reduxThunk from 'redux-thunk'
 
 import reducer from './reducer'
+import optimisticMiddleware from '../middleware/optimisticMiddleware'
 import * as messageActions from './actions/message'
 import * as userActions from './actions/user'
 
-const store = createStore(reducer, applyMiddleware(reduxThunk))
+const store = createStore(reducer, applyMiddleware(reduxThunk, optimisticMiddleware))
 
 export const MessageActions = bindActionCreators(messageActions, store.dispatch)
 export const UserActions = bindActionCreators(userActions, store.dispatch)
